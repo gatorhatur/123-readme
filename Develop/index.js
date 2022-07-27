f5086d4081acce3f1e2b9a5d113e92af37a9a200
Brandon Davis <brandon.davis@conehealth.com>
2022-07-27 12:36:00 -0400

    started framing questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+cost fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        question: 'What is the title of the project/repo? (Required)',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            }
+            else {
+                console.log("Please enter a name!");
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        question: 'Provide a description of the project. (Required)',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            }
+            else {
+                console.log("Please enter a name!");
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installInstructions',
+        question: 'Provide any installation instructions that a user would need to use your app. (Required)',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        question: 'Provide any usage information',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        question: 'What license is this project covered under?',
+        choices: ['MIT',],
+        default: 'MIT'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
