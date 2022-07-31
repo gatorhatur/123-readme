19105e60249ef9c39728c677a646392b01c615da
Brandon Davis <brandon.davis@conehealth.com>
2022-07-31 13:24:52 -0400

    work on functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -66,17 +66,7 @@ function generateMarkdown(data) {
   
   If you followed tutorials, include links to those here as well.
   
-  ${renderLicenseSection}
-  
-  ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  ${renderLicenseSection(data.license)}
   
   ## Features
   
